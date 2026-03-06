9a9d79c17756ead1c3d6e0c48358cd80b565a3aa
Tamilselvan M <tamilselvanmurugan2005@gmail.com>
2026-03-06 11:44:16 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,24 +1,9 @@
-from transformers import MarianMTModel, MarianTokenizer
+from deep_translator import GoogleTranslator
 
-
-class Translator:
-
-    def __init__(self):
-
-        model_name = "Helsinki-NLP/opus-mt-mul-en"
-
-        self.tokenizer = MarianTokenizer.from_pretrained(model_name)
-        self.model = MarianMTModel.from_pretrained(model_name)
-
-    def translate_to_english(self, text):
-
-        inputs = self.tokenizer(text, return_tensors="pt", padding=True)
-
-        translated = self.model.generate(**inputs)
-
-        output = self.tokenizer.decode(
-            translated[0],
-            skip_special_tokens=True
-        )
-
-        return output
+def translate_to_english(text):
+    try:
+        translated = GoogleTranslator(source='auto', target='en').translate(text)
+        return translated
+    except Exception as e:
+        print("Translation Error:", e)
+        return text
